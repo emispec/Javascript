32965c7fb0e7d75c3793863df94089701cc5ebfe
emispec <emisebastian86@gmail.com>
2022-09-06 13:00:40 -0300

    update r&m app search

--- JS-rick&morty(API)/script.js
+++ JS-rick&morty(API)/script.js
@@ -3,12 +3,34 @@
 // Base url: https://rickandmortyapi.com/api
 
 const url = "https://rickandmortyapi.com/api/character";
+const form = document.getElementById("form");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const input = document.getElementById("search").value.toLowerCase();
+  const data = JSON.parse(localStorage.getItem("characters"));
+
+  if (input) {
+    fetch(url)
+      .then((response) => response.json())
+      .then((characters) =>
+        localStorage.setItem("characters", JSON.stringify(characters.results))
+      );
+  }
+
+  const newArray =
+    data && data.flatMap((item) => ({ name: item.name.toLowerCase() }));
+  const nameFilter = newArray.filter((item) => item.name == input);
+  console.log(newArray);
+  console.log(nameFilter);
+});
 
 window.addEventListener("load", () => {
   const character = document.getElementById("characters");
   const ul = document.createElement("ul");
   const data = JSON.parse(localStorage.getItem("characters"));
 
+  /*
   const getData = () => {
     return fetch(url)
       .then((response) => response.json())
@@ -39,4 +61,5 @@ window.addEventListener("load", () => {
       ul.appendChild(li);
       character.appendChild(ul);
     });
+    */
 });
