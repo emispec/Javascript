09da6acd6eec67ccb5a745b64488a531aad1b93f
emispec <emisebastian86@gmail.com>
2022-09-06 19:53:21 -0300

    rick&morty app (API)

--- JS-rick&morty(API)/script.js
+++ JS-rick&morty(API)/script.js
@@ -5,61 +5,36 @@
 const url = "https://rickandmortyapi.com/api/character";
 const form = document.getElementById("form");
 
-form.addEventListener("submit", (event) => {
+form.addEventListener("submit", async (event) => {
   event.preventDefault();
-  const input = document.getElementById("search").value.toLowerCase();
-  const data = JSON.parse(localStorage.getItem("characters"));
-
-  if (input) {
-    fetch(url)
-      .then((response) => response.json())
-      .then((characters) =>
-        localStorage.setItem("characters", JSON.stringify(characters.results))
-      );
-  }
-
-  const newArray =
-    data && data.flatMap((item) => ({ name: item.name.toLowerCase() }));
-  const nameFilter = newArray.filter((item) => item.name == input);
-  console.log(newArray);
-  console.log(nameFilter);
-});
-
-window.addEventListener("load", () => {
   const character = document.getElementById("characters");
   const ul = document.createElement("ul");
-  const data = JSON.parse(localStorage.getItem("characters"));
+  const input = document.getElementById("search").value.toLowerCase();
+  let data = [];
 
-  /*
-  const getData = () => {
-    return fetch(url)
-      .then((response) => response.json())
-      .then((characters) =>
-        localStorage.setItem(
-          "characters",
-          JSON.stringify(
-            characters.results.flatMap((item) => ({
-              name: item.name,
-              image: item.image,
-            }))
-          )
-        )
-      );
-  };
+  if (input) {
+    let response = await fetch(url);
+    let names = await response.json();
+    data = names.results;
+  }
 
-  getData();
+  const newArray = data.flatMap((item) => ({
+    name: item.name.toLowerCase(),
+    image: item.image,
+  }));
 
-  data &&
-    data.map((item) => {
+  const filterArray = newArray.filter((item) => item.name === input);
+
+  filterArray &&
+    filterArray.map((item) => {
       const li = document.createElement("li");
       const img = document.createElement("img");
       const span = document.createElement("span");
       span.appendChild(document.createTextNode(item.name));
-      li.appendChild(span);
       img.src = item.image;
       li.appendChild(img);
+      li.appendChild(span);
       ul.appendChild(li);
       character.appendChild(ul);
     });
-    */
 });
